5c0f7aa9e53a3eec1ef7f5da82eb7504be1f9449
Alemapyapur <pamela.rupay31@gmail.com>
2021-07-07 23:11:13 -0500

    style

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,150 @@
-import { example, anotherExample } from '../src/data.js';
+import { selectName,selectFilter, selectWeak, orderPokes,calc  } from '../src/data.js';
 
+const pokemonMock = {
+  pokemon: [{
+    "name": "Bulbasaur",
+    "type": [
+      "Grass",
+      "Poison"
+      
+    ],
+    "size":{
+      "height": "0.71 m",
+    },
+  
+    "spawn_chance": 0.69,
+    "weaknesses": [
+      "Fire",
+      "Ice",
+      "Flying",
+      "Psychic"
+    ]
+  },
+  {
+    "name": "Ivysaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "size":{
+      "height": "0.99 m",
+    },
+    
+    "spawn_chance": 0.042,
+    "weaknesses": [
+      "Fire",
+      "Ice",
+      "Flying",
+      "Psychic"
+    ]
+  },
+  {
+    "name": "Squirtle",
+    "type": [
+      "Water"
+    ],
+    "size":{
+      "height": "0.51 m", 
+    },
+    
+    "spawn_chance": 0.58,
+    "weaknesses": [
+      "Electric",
+      "Grass"
+    ],
+  }]
+};
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('Selecting a pokemon by name', () => {
+  it('should be a function', () => {
+    expect(typeof selectName).toBe('function');
   });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('should return "Bulbasaur" when search the pokemon by name "bul"', () => {
+    const result = selectName(pokemonMock.pokemon, "bul")
+    expect(result[0].name).toEqual("Bulbasaur")
   });
 });
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe('Selecting a pokemon by type', () => {
+  it('should be a function', () => {
+    expect(typeof selectFilter).toBe('function');
   });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('should return "Squirtle", for type "Water"', () => {
+    const result = selectFilter(pokemonMock.pokemon, "Water", "type")
+    expect(result[0].name).toEqual("Squirtle")
   });
 });
+
+describe('Showing the percentage of the type', () => {
+  it('should be a function', () => {
+    expect(typeof calc).toBe('function');
+  });
+  it('shold return 66.67, for type "Grass"', () => {
+    const result = calc(pokemonMock.pokemon, "Grass", "type")
+    expect(result).toEqual(66.67)
+  });
+});
+
+describe('Selecting a pokemon by weakness', () => {
+  it('should be a function', () => {
+    expect(typeof selectWeak).toBe('function');
+  });
+  it('should return "Squirtle" for Electric"', () => {
+    const result = selectWeak(pokemonMock.pokemon, "Electric")
+    expect(result[0].name).toEqual("Squirtle")
+  });
+});
+describe('Selecting a pokemon by order', () => {
+  it('should be a function', () => {
+    expect(typeof orderPokes).toBe('function');
+  });
+  it('should return "Bulbasaur", "Ivysaur" and "Squirtle" by "A-Z" order', () => {
+    const result = orderPokes(pokemonMock.pokemon, "order-az")
+    console.log(result,"lenght");
+    expect(result).toEqual(pokemonMock.pokemon)
+  });
+  it('should return "Squirtle", "Ivysaur" and "Bulbasaur" by "Z-A" order', () => {
+    const result = orderPokes(pokemonMock.pokemon, "order-za")
+    expect(result).toEqual(pokemonMock.pokemon)
+  });
+  it('should return "Squirtle", "Bulbasaur" and "Ivysaur" by "SIZE" order', () => {
+    const result = orderPokes(pokemonMock.pokemon, "size")
+    expect(result).toEqual(pokemonMock.pokemon)
+  });
+  it('should return "Ivysaur","Bulbasaur" and "Squirtle" by "SIZE-REVERSE" order', () => {
+    const result = orderPokes(pokemonMock.pokemon, "size-reverse")
+    expect(result).toEqual(pokemonMock.pokemon)
+  });
+  it('should return "Bulbasaur" ,"Squirtle" and "Ivysaur" by "ORDER-SPAWN" order', () => {
+    const result = orderPokes(pokemonMock.pokemon, "order-spawn")
+    expect(result).toEqual(pokemonMock.pokemon)
+  });
+})
+
+
+
+
+
+
+
+
+//describe('example', () => {
+  //it('is a function', () => {
+   // expect(typeof example).toBe('function');
+  //});
+
+  //it('returns `example`', () => {
+   // expect(example()).toBe('example');
+  //});
+//});
+
+
+//describe('anotherExample', () => {
+  //it('is a function', () => {
+    //expect(typeof anotherExample).toBe('function');
+  //});
+
+  //it('returns `anotherExample`', () => {
+  //  expect(anotherExample()).toBe('OMG');
+  //});
+//});
